e1ab2a615540e7f9b6924addb90c493d14618927
CrazyCase2473 <deacon.mc08@gmail.com>
2026-02-20 13:21:30 -0600

    Access Record struct made

--- Program.cs
+++ Program.cs
@@ -7,3 +7,22 @@ class Program
         Console.WriteLine("Hello, World!");
     }
 }
+
+public struct AccessRecord
+{
+    public string DisplayName { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string WorkEmail { get; set; }
+    public string CloudLifecycleState { get; set; }
+    public string IdentityID { get; set; }
+    public string IsManager { get; set; }
+    public string Department { get; set; }
+    public string JobTitle { get; set; }
+    public string Uid { get; set; }
+    public string AccessType { get; set; }
+    public string AccessSourceName { get; set; }
+    public string AccessDisplayName { get; set; }
+    public string AccessDescription { get; set; }
+}
+
