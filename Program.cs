bedcaeb4dcffacf3e327fb62d4b37f939b4beca0
CrazyCase2473 <deacon.mc08@gmail.com>
2026-02-20 13:47:53 -0600

    CSV reader without the booleans

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
-﻿namespace Reading_csv;
+﻿using System.Reflection.Metadata.Ecma335;
+using Microsoft.Win32.SafeHandles;
+
+namespace Reading_csv;
 
 class Program
 {
@@ -14,9 +17,9 @@ public struct AccessRecord
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public string WorkEmail { get; set; }
-    public string CloudLifecycleState { get; set; }
+    public bool CloudLifecycleState { get; set; }
     public string IdentityID { get; set; }
-    public string IsManager { get; set; }
+    public bool IsManager { get; set; }
     public string Department { get; set; }
     public string JobTitle { get; set; }
     public string Uid { get; set; }
@@ -26,3 +29,38 @@ public struct AccessRecord
     public string AccessDescription { get; set; }
 }
 
+public class CsvReader
+{
+    public static List<AccessRecord> Read(string filePath)
+    {
+        List<AccessRecord> records = new List<AccessRecord>();
+        using (StreamReader s = new StreamReader(filePath))
+        {
+            string headerline = s.ReadLine();
+
+            while (!s.EndOfStream)
+            {
+                string line = s.ReadLine();
+                string[] values = line.Split(",");
+                AccessRecord record = new AccessRecord
+                {
+                    DisplayName = values[0],
+                    FirstName = values[1],
+                    LastName = values[2],
+                    WorkEmail = values[3],
+                    IdentityID = values[5],
+                    Department = values[7],
+                    JobTitle = values[8],
+                    Uid = values[9],
+                    AccessType = values[10],
+                    AccessSourceName = values[11],
+                    AccessDisplayName = values[12],
+                    AccessDescription = values[13],
+
+                };
+                records.Add(record);
+            }
+        }
+        return records;
+    }
+}
